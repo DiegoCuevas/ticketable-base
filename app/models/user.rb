d4e14851f7aa3586942f094b82c93629e9104e5f
Arturo Puente <arturo@able.co>
2019-04-17 01:15:01 -0500

    Add role helper methods to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def member?
+    self.role == "member"
+  end
+
+  def premium?
+    self.role == "premium"
+  end
+
+  def admin?
+    self.role == "admin"
+  end
 end
