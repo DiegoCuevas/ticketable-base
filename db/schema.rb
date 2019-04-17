99a0594a3f436e630761158fec619d322fd27cd9
Arturo Puente <arturo@able.co>
2019-04-17 00:50:27 -0500

    Add simple ticket model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_053802) do
+ActiveRecord::Schema.define(version: 2019_04_17_054541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "movie_name"
+    t.string "seat"
+    t.string "tier"
+    t.integer "price"
+    t.boolean "available", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
